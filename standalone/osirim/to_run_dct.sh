b9faf9c08b5518947d38e3d589ffe8a154d4da1c
leocances <leocances@gmail.com>
2020-11-03 12:06:49 +0100

    dct-mse cross-validation

--- standalone/osirim/to_run_dct.sh
+++ standalone/osirim/to_run_dct.sh
@@ -1,70 +1,11 @@
-# === === === === === === === === === === === === === === === === === === === === ===
-# Urbansound8k
-# === === === === === === === === === === === === === === === === === === === === ===
-# bash deep-co-training.sh cnn03 ubs8k
-# bash deep-co-training.sh resnet18 ubs8k
-# bash deep-co-training.sh resnet34 ubs8k
-# bash deep-co-training.sh resnet50 ubs8k
-# bash deep-co-training.sh wideresnet28_2 ubs8k -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_2 ubs8k -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_8 ubs8k -p RTX6000Node -g 2
+c_args="--lambda_cot_max 1 --lambda_diff_max 0.5 --warmup_length 160 --learning_rate 0.0005 --epoch 300 --batch_size 100"
 
-# === === === === === === === === === === === === === === === === === === === === ===
-# ESC10
-# === === === === === === === === === === === === === === === === === === === === ===
-# bash deep-co-training.sh cnn03 esc10
-# bash deep-co-training.sh resnet18 esc10
-# bash deep-co-training.sh resnet34 esc10
-# bash deep-co-training.sh resnet50 esc10
-# bash deep-co-training.sh wideresnet28_2 esc10 -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_2 esc10 -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_8 esc10 -p RTX6000Node -g 2
+bash deep-co-training.sh wideresnet28_2 ubs8k ${c_args} --ratio 0.10 -C
+bash deep-co-training.sh wideresnet28_2 ubs8k ${c_args} --ratio 0.10 --script ../co-training/co-training_mse.py -C
 
-# === === === === === === === === === === === === === === === === === === === === ===
-# ESC50
-# === === === === === === === === === === === === === === === === === === === === ===
-# bash deep-co-training.sh cnn03 esc50
-# bash deep-co-training.sh resnet18 esc50
-# bash deep-co-training.sh resnet34 esc50
-# bash deep-co-training.sh resnet50 esc50
-# bash deep-co-training.sh wideresnet28_2 esc50 -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_4 esc50 -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_8 esc50 -p RTX6000Node -g 2
+bash deep-co-training.sh wideresnet28_2 esc10 ${c_args} --ratio 0.10 -C
+bash deep-co-training.sh wideresnet28_2 esc10 ${c_args} --ratio 0.10 --script ../co-training/co-training_mse.py -C
 
-
-# === === === === === === === === === === === === === === === === === === === === ===
-# SPEECHCOMMAND
-# === === === === === === === === === === === === === === === === === === === === ===
-# bash deep-co-training.sh cnn03 speechcommand
-# bash deep-co-training.sh resnet18 speechcommand
-# bash deep-co-training.sh resnet34 speechcommand
-# bash deep-co-training.sh resnet50 speechcommand
-# bash deep-co-training.sh wideresnet28_2 speechcommand
-# bash deep-co-training.sh wideresnet28_4 speechcommand
-# bash deep-co-training.sh wideresnet28_8 speechcommand -p RTX6000Node
-
-
-
-
-
-
-# === === === === === === === === === === === === === === === === === === === === ===
-# CUSTOM
-# === === === === === === === === === === === === === === === === === === === === ===
-c_args="--lambda_cot_max 1 --lambda_diff_max 0.5 --warmup_length 160 --learning_rate 0.0005 --epoch 300"
-# 
-d_args="--batch_size 100"
-bash deep-co-training.sh wideresnet28_2 esc10 -p RTX6000Node ${c_args} ${d_args} --ratio 0.10 -C
-bash deep-co-training.sh wideresnet28_2 esc10 -p RTX6000Node ${c_args} ${d_args} --ratio 0.25 -C
-bash deep-co-training.sh wideresnet28_2 esc10 -p RTX6000Node ${c_args} ${d_args} --ratio 0.50 -C
-# 
-d_args="--batch_size 100"
-bash deep-co-training.sh wideresnet28_2 ubs8k -p RTX6000Node ${c_args} ${d_args} --ratio 0.10 -C
-bash deep-co-training.sh wideresnet28_2 ubs8k -p RTX6000Node ${c_args} ${d_args} --ratio 0.25 -C
-bash deep-co-training.sh wideresnet28_2 ubs8k -p RTX6000Node ${c_args} ${d_args} --ratio 0.50 -C
-# 
-d_args="--batch_size 256"
-# bash deep-co-training.sh wideresnet28_2 SpeechCommand ${c_args} ${d_args} --seed $i --ratio 0.10 -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_2 SpeechCommand ${c_args} ${d_args} --seed $i --ratio 0.25 -p RTX6000Node
-# bash deep-co-training.sh wideresnet28_2 SpeechCommand ${c_args} ${d_args} --seed $i --ratio 0.50 -p RTX6000Node
+# bash deep-co-training.sh wideresnet28_2 SpeechCommand ${c_args} --ratio 0.10 
+# bash deep-co-training.sh wideresnet28_2 SpeechCommand ${c_args} --ratio 0.10 --script ../co-training/co-training_mse.py
 
