9a120b73b93982c9f17189168b8b012e5d297bdd
leocances <leocances@gmail.com>
2020-11-24 15:35:37 +0100

    fix a reproductability issue (benchmark was set to True

--- SSL/util/utils.py
+++ SSL/util/utils.py
@@ -93,7 +93,7 @@ def reset_seed(seed):
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
 
 
 class ZipCycle(Iterable, Sized):
