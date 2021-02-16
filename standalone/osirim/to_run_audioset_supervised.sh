50eef8e9e1d7dcdbe0bd122d2ea366bfd160282e
leocances <leocances@gmail.com>
2021-02-16 11:21:15 +0100

    change batchsize to 128 and nb_cpu to 10

--- standalone/osirim/to_run_audioset_supervised.sh
+++ standalone/osirim/to_run_audioset_supervised.sh
@@ -1,15 +1,15 @@
 # /!\ BATCH SIZE IS DOUBLED BECAUSE RUNNING ON TWO GPU !!, if one GPU, use 32
 
-common_args="--dataset audioset-unbalanced --supervised_ratio 1.0 --nb_epoch 15 --batch_size 64 --learning_rate 0.003 --seed 1234"
+common_args="--dataset audioset-unbalanced --supervised_ratio 1.0 --nb_epoch 15 --batch_size 128 --learning_rate 0.003 --seed 1234"
 mixup_args="--mixup --mixup_alpha 0.4 --mixup_label"
 specaugment_args="--specAugment --sa_time_drop_width 32 --sa_time_stripes_num 1 --sa_freq_drop_width 4 --sa_freq_stripes_num 1"
 args="$common_args $mixup_args $specaugment_args"
 
 # all together
-# bash supervised-multi-label.sh --model wideresnet28_2 -g 2 -p GPUNodes $args
+bash supervised-multi-label.sh --model wideresnet28_2 -c 10 -g 2 -p GPUNodes $args
 
 # # mixup only
-bash supervised-multi-label.sh --model wideresnet28_2 -g 2 -p GPUNodes $common_args $mixup_arsg
+# bash supervised-multi-label.sh --model wideresnet28_2 -c 10 -g 2 -p GPUNodes $common_args $mixup_arsg
  
 # # specaugment only
-bash supervised-multi-label.sh --model wideresnet28_2 -g 2 -p RTX6000Node $common_args $specaugment_args
+# bash supervised-multi-label.sh --model wideresnet28_2 -c 10 -g 2 -p RTX6000Node $common_args $specaugment_args
