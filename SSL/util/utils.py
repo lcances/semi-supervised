269b327253a689054387c989e1962d856886031c
leocances <leocances@gmail.com>
2021-02-23 15:36:34 +0100

    add a new cache system more flexible (can be deactivate during runtime)

--- SSL/util/utils.py
+++ SSL/util/utils.py
@@ -7,6 +7,7 @@ import time
 from collections import Iterable, Sized
 from zipfile import ZipFile, ZIP_DEFLATED 
 import os
+from typing import Callable
 
 # TODO write q timer decorator that deppend on the logging level
 
@@ -45,6 +46,33 @@ def conditional_cache_v2(func):
     decorator.cache = dict()
 
     return decorator
+
+
+class Cacher:
+    def __init__(self, func: Callable):
+        if func is None:
+            return None
+        
+        self.func = func
+        self.cached_func = self.cache_wrapper(func)
+        
+    def __call__(self, *args, caching: bool = True, **kwargs):
+        if caching:
+            return self.cached_func(*args, **kwargs)
+        
+        return self.func(*args, **kwargs)
+        
+    def cache_wrapper(self, func):
+        def decorator(*args, **kwargs):
+            key = ",".join(map(str, args))
+
+            if key not in decorator.cache:
+                decorator.cache[key] = func(*args, **kwargs)
+
+            return decorator.cache[key]
+
+        decorator.cache = dict()
+        return decorator
 
 
 def track_maximum():
