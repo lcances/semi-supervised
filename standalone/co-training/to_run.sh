2afdd9e7bf2d579310a5e2a7fe9ac4f2649a6f0c
leocances <leocances@gmail.com>
2021-03-15 14:50:33 +0100

    hydra ready deep co training

--- standalone/co-training/to_run.sh
+++
@@ -1,8 +0,0 @@
-# python co-training.py --dataset ubs8k --model wideresnet28_2 -t 1 2 3 4 5 6 7 8 9 -v 10 --num_classes 10
-# python co-training_mse.py --dataset ubs8k --model wideresnet28_2 -t 1 2 3 4 5 6 7 8 9 -v 10 --num_classes 10
-
-# python co-training.py --dataset esc10 --model wideresnet28_2 -t 1 2 3 4 -v 5 --num_classes 10
-# python co-training_mse.py --dataset esc10 --model wideresnet28_2 -t 1 2 3 4 -v 5 --num_classes 10
-
-python co-training.py --dataset SpeechCommand --model wideresnet28_2 --num_classes 35
-python co-training_mse.py --dataset SpeechCommand --model wideresnet28_2 --num_classes 35
