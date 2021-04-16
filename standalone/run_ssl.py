5c78e6841dcc590fb0154e225883e16685cb6f91
leocances <leocances@gmail.com>
2021-04-16 11:30:40 +0200

    can now run any method (mean teacher, fixmatch, dct, supervised) with any dataset

--- standalone/run_ssl.py
+++ standalone/run_ssl.py
@@ -9,6 +9,7 @@ from torchaudio import load as ta_load
 from SSL.util.checkpoint import CheckPoint
 from SSL.util.model_loader import load_model
 from SSL.util.loaders import load_optimizer, load_preprocesser
+from SSL.util.utils import DotDict
 
 
 available_methods = [
@@ -28,15 +29,33 @@ available_models = [
     'MobileNetV1', 'MobileNetV2']
 
 
-nb_class = {
-    'ubs8k': 10,
-    'esc10': 10,
-    'esc50': 50,
-    'speechcommand': 35,
-    'compare2021-prs': 5,
-    'audioset-unbalanced': 527,
-    'audioset-balanced': 527,
-}
+dataset_info = DotDict({
+    'script': DotDict({
+        'ComParE2021-PRS'.lower(): 'compare2021-prs.py',
+        'audioset-unbalanced': 'audioset.py',
+        'audioset-balanced': 'audioset.py',
+    }),
+    
+    'config': DotDict({
+        'ComParE2021-PRS'.lower(): 'compare2021-prs.yaml',
+        'esc10': 'esc10.yaml',
+        'speechcommand': 'speechcommand.yaml',
+        'ubs8k': 'ubs8k.yaml',
+        'audioset-unbalanced': 'audioset.yaml',
+        'audioset-balanced': 'audioset.yaml',
+    }),
+        
+    'nb_class': DotDict({
+        'ubs8k': 10,
+        'esc10': 10,
+        'esc50': 50,
+        'speechcommand': 35,
+        'compare2021-prs': 5,
+        'audioset-unbalanced': 527,
+        'audioset-balanced': 527,
+    })
+})
+
 
 ubs8k_folds = [
         [[1,2,3,4,5,6,7,8,9],[10,]],
@@ -58,17 +77,20 @@ esc_folds = [
         [[5,1,2,3], [4,]],
 ]
 
+
+
 def get_script_path(method: str, dataset: str) -> str:
-    path = 'supervised'
-
-    if dataset.lower() == 'ComParE2021-PRS'.lower():
-        return os.path.join(path, 'compare2021-prs.py')
-
-    elif dataset.lower() == 'audioset'.lower():
-        return os.path.join(path, 'audioset.py')
-
+    path = method
+    
+    if dataset.lower() in dataset_info.script.keys():
+        return os.path.join(path, dataset_info.script[dataset.lower()])
+    
     else:
         return os.path.join(path, method + '.py')
+    
+    
+def get_config_path(method: str, dataset: str) -> str:
+    return os.path.join('./../..', 'config', method, dataset_info.config[dataset.lower()])
 
 
 def list_file_per_date(path: str) -> list:
@@ -81,7 +103,7 @@ def list_file_per_date(path: str) -> list:
 def train(args):
     '''Perform training for the specified model on the specified dataset.'''
     # Automatically load the configuration file
-    config_path = os.path.join('./../..', 'config', args.method, args.dataset.lower() + '.yaml')
+    config_path = get_config_path(args.method, args.dataset)
     script_path = get_script_path(args.method, args.dataset)
 
     print('Default training parameters at: ', config_path)
@@ -90,8 +112,8 @@ def train(args):
     # Prepare the parameters to override
     override_hydra_params = args.kwargs
     override_hydra_params += ['path.dataset_root=../datasets']
-    override_hydra_params += ['path.checkpoint_root=../model_save']
-    override_hydra_params += ['path.tensorboard_root=../tensorboard']
+    override_hydra_params += ['path.logs_root=../']
+#     override_hydra_params += ['path.tensorboard_root=../tensorboard']
 
     subprocess_params = ['python', script_path] + ['-cn', config_path] + override_hydra_params
     subprocess.call(subprocess_params)
@@ -109,8 +131,9 @@ def cross_validation(args):
     # Prepare the parameters to override
     override_hydra_params = args.kwargs
     override_hydra_params += ['path.dataset_root=../datasets']
-    override_hydra_params += ['path.checkpoint_root=../model_save']
-    override_hydra_params += ['path.tensorboard_root=../tensorboard']
+    override_hydra_params += ['path.logs_root=../']
+#     override_hydra_params += ['path.checkpoint_root=../model_save']
+#     override_hydra_params += ['path.tensorboard_root=../tensorboard']
 
     # prepare the set of run
     if args.dataset == 'ubs8k': folds = ubs8k_folds
